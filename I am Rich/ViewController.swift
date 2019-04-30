dfd475fe85e970f15277a37f6878d6b17e2920f0
Matthew W P Barber <barberwmatt@Edwards-Mac-mini.local>
2019-04-29 21:48:06 -0400

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Matthew W P Barber on 4/29/19.
+//  Copyright © 2019 Matthew W P Barber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
